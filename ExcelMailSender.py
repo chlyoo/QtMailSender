2e30d33a3a119419682231af21b290a548c61b8b
chlyoo <changhyunlyoo@gmail.com>
2021-04-28 16:01:50 +0900

    Threading

--- ExcelMailSender.py
+++ ExcelMailSender.py
@@ -3,17 +3,21 @@ import pandas as pd
 import smtplib
 from email.mime.text import MIMEText
 from config import * 
+from threading import Thread
 
-def send_score(who, msg):
+def send_async_email(who, msg):
 	msg = MIMEText(msg)
-	msg['Subject'] = '글로컬시사영어 중간시험 성적 안내'
+	msg['Subject'] = MAIL_SUBJECT
 	s.sendmail(MAIL_SENDER, who, msg.as_string())
-	s.quit()
+	# print(f"{who}")
 
 if __name__ == '__main__':
 	df = pd.read_excel("data.xlsx", header=0 )
 	s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-	# s.login(MAIL_SENDER, MAIL_PASSWORD)
+	s.login(MAIL_SENDER, MAIL_PASSWORD)
 	for index, row in df.iterrows():
-		print(row['이메일 주소'], f"간호학과 {row['성명을 쓰시오.']} {row['점수']}/65 ")
-		# send_score(row['이메일 주소'], f"간호학과 {row['성명을 쓰시오.']} {row['점수']}/65 ")
+		who = row['이메일 주소']
+		msg = f"간호학과 {row['성명을 쓰시오.']} {row['점수']}/65 "
+		thr = Thread(target=send_async_email, args=[who,msg])
+		thr.start()
+	s.quit()
